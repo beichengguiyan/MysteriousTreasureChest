9be3b2637e4667842289a94f447f2fc0e2d67473
beichengguiyan <beichengguiyan@gmail.com>
2020-12-31 16:29:16 +0800

    hould_return_false_when_given_number_222()

--- src/main/java/com/game/Box.java
+++ src/main/java/com/game/Box.java
@@ -1,5 +1,11 @@
 package com.game;
 
+
+import com.sun.xml.internal.ws.util.StringUtils;
+
+import java.io.UnsupportedEncodingException;
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
 import java.util.Date;
 
 /**
@@ -7,27 +13,57 @@ import java.util.Date;
  */
 public class Box {
     private int id;
-    private Date date;
-    private int hashValue;
-    private int previousHashValue;
+    private String hashValue;
+    private String previousHashValue;
     private int mysteriousNumber;
+    public static final String CHECK = "00000";
 
-    public Box(int id, int previousHashValue) {
+    public Box(int id, String previousHashValue) {
         this.id = id;
         this.previousHashValue = previousHashValue;
     }
 
 
-    public boolean isOpen(int number) {
-
+    public boolean tryOpen(int mysteriousNumber) throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        setMysteriousNumber(mysteriousNumber);
+        calculateHashValue();
+        if (isOpen()) {
+            return true;
+        }
         return false;
     }
 
-    public void setDate(Date date) {
-        this.date = new Date();
+    public boolean isOpen() {
+        if (CHECK.equals(hashValue.substring(0, 5))) {
+            return true;
+        }
+        return false;
+    }
+
+    public void calculateHashValue() throws UnsupportedEncodingException, NoSuchAlgorithmException {
+        String hashString = this.id + this.previousHashValue + this.mysteriousNumber;
+        setHashValue(sha256(hashString));
+    }
+
+    public void setHashValue(String hashValue) {
+        this.hashValue = hashValue;
+    }
+
+    public String getHashValue() {
+        return hashValue;
     }
 
     public void setMysteriousNumber(int mysteriousNumber) {
         this.mysteriousNumber = mysteriousNumber;
+    }
+
+    public static String sha256(String s) throws NoSuchAlgorithmException, UnsupportedEncodingException {
+        StringBuilder sb = new StringBuilder();
+        MessageDigest messageDigest = MessageDigest.getInstance("SHA-256");
+        byte[] encrypted = messageDigest.digest(s.getBytes("UTF-8"));
+        for (byte b : encrypted) {
+            sb.append(String.format("%02x", b));
+        }
+        return sb.toString();
     }
 }
--- src/test/java/com/game/BoxTest.java
+++ src/test/java/com/game/BoxTest.java
@@ -2,16 +2,30 @@ package com.game;
 
 import org.junit.Test;
 
+import java.io.UnsupportedEncodingException;
+import java.security.NoSuchAlgorithmException;
+
 import static org.junit.Assert.*;
 
 public class BoxTest {
+
+    /**
+     * 测试构建box时，尝试开箱的操作
+     * 如果: given 创建对象box和测试的神秘数字
+     * 当:  when 调用创建对象的方法时
+     * 则:  then 品质依然为50
+     */
     @Test
-    public void should_return_10_when() {
+    public void should_return_false_when_given_number_222() throws UnsupportedEncodingException, NoSuchAlgorithmException {
         //given
-        Box box = new Box(0, 0);
+        Box box = new Box(0, "0");
         int number = 222;
+
         //when
-       boolean actual =  box.isOpen(number);
+        boolean actual = box.tryOpen(number);
+
+        //then
+        assertFalse(actual);
     }
 
 }
